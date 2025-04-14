3fd031a479c7c5d80e75e894c26e73ad668f7f71
DuyTinNguyen182 <duytinnguyen.2004@gmail.com>
2025-04-14 09:06:16 +0700

    Add image of Duy Tin

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function showMembers(){
-    const members = ["Nguyễn Duy Tín", "Kiều Gia Thịnh", "Lý Thanh Tâm"];
-    document.getElementById("memberList").innerHTML=members.join("<br>");
+function showMembers() {
+    const members = [
+        { name: "Nguyễn Duy Tín", img: "imgs/avt_tin.jpg" },
+        { name: "Kiều Gia Thịnh", img: "imgs/avt_thinh.jpg" },
+        { name: "Lý Thanh Tâm", img: "imgs/avt_tam.jpg" }
+    ];
+
+    const container = document.getElementById("memberList");
+    container.innerHTML = "";
+
+    members.forEach(member => {
+        const memberDiv = document.createElement("div");
+        memberDiv.style.marginBottom = "20px";
+
+        const img = document.createElement("img");
+        img.src = member.img;
+        img.alt = member.name;
+        img.style.width = "100px";
+        img.style.display = "block";
+
+        const name = document.createElement("p");
+        name.textContent = member.name;
+
+        memberDiv.appendChild(img);
+        memberDiv.appendChild(name);
+        container.appendChild(memberDiv);
+    });
 }
